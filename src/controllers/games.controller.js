a1500b0fddf0cd353472a9f7180efd5700ab38fe
caiohbe <caiohbe@gmail.com>
2022-12-10 17:47:29 -0300

    feat: get /games query strings

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,11 +1,27 @@
 import connectionDB from "../database/db.js"
 
 export async function getGames(req, res) {
+    let games
+
     try {
-        const games = await connectionDB.query("SELECT * FROM games;")
-        res.send(games.rows)
-        
+        games = await connectionDB.query("SELECT * FROM games;")
     } catch (err) {
         res.status(500).send(err.message)
     }
+
+    
+    if (req.query.name) {
+        const name = req.query.name.toLowerCase()
+    
+        const result = games.rows.filter((g) => {
+            if (g.name.toLowerCase().includes(name)) {
+                return true
+            }
+        })
+
+        res.send(result)
+        return
+    }
+    
+    res.send(games.rows)
 }
