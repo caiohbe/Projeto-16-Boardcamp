2edd4b2f904357c771a6a67877325d7a33afaa5e
caiohbe <caiohbe@gmail.com>
2022-12-11 16:11:32 -0300

    feat: get /customers

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,1 +1,26 @@
 import connectionDB from "../database/db.js"
+
+export async function getCustomers(req, res) {
+    let customers 
+
+    try {
+        customers = connectionDB.query("SELECT * FROM customers;")
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+    if (req.query.name) {
+        const cpf = req.query.cpf
+    
+        const result = customers.rows.filter((c) => {
+            if (c.cpf.includes(cpf)) {
+                return true
+            }
+        })
+
+        res.send(result)
+        return
+    }
+
+    res.send(customers.rows)
+}
--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getCustomers } from "../controllers/customer.controller.js";
 
 const router = Router()
+
+router.get("/customers", getCustomers)
 
 export default router
